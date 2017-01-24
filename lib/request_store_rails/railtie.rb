1b2e25a7d7cbfb6caa84ea9c178fc06c70d9c30b
Aymeric Brisse <aymeric.brisse@gmail.com>
2017-01-24 16:08:08 +0100

    Remove Rails deprecation warning

--- lib/request_store_rails/railtie.rb
+++ lib/request_store_rails/railtie.rb
@@ -7,8 +7,12 @@ module RequestStoreRails
     initializer 'request_store_rails.insert_middleware' do |app|
       app.config.middleware.insert_after ActionDispatch::RequestId, RequestStoreRails::Middleware
 
-      ActionDispatch::Reloader.to_cleanup do
-        RequestLocals.clear_all!
+      clear_all = -> { RequestLocals.clear_all! }
+
+      if defined?(ActiveSupport::Reloader)
+        ActiveSupport::Reloader.to_complete(&clear_all)
+      else
+        ActionDispatch::Reloader.to_cleanup(&clear_all)
       end
     end
   end
